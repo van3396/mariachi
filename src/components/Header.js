9c93120da187fd41529fc379476e65c2008994ea
Qui <quivan3396@yahoo.com>
2021-04-28 14:18:31 -0400

    fix: font style and special spanish n character for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,18 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
   const ytLogo =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg2b4AzEL5ltvxPV_DbclqmJhSdtTQgiXmQ_fk-GDlZwsSAhfuhp1hIHoJziYAA-1YRZY&usqp=CAU"  
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQg2b4AzEL5ltvxPV_DbclqmJhSdtTQgiXmQ_fk-GDlZwsSAhfuhp1hIHoJziYAA-1YRZY&usqp=CAU";
   return (
     <header>
       <Navbar className="header-bg-color py-1" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand className="header_brand" href="/">Mariachi Veracruzano</Navbar.Brand>
+          <Navbar.Brand className="header_brand" href="/">
+            Mariachi Veracruzano
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="#spanish">Espanol</Nav.Link>
+              <Nav.Link href="#spanish">Espa&ntilde;ol</Nav.Link>
               <Nav.Link href="#english">English</Nav.Link>
               <Nav.Link
                 href="https://www.youtube.com/"
