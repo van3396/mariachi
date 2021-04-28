6104c4d5500eae82b8a4a0c6357d48b69e8ab699
Qui <quivan3396@yahoo.com>
2021-04-28 11:14:03 -0400

    feat: setup header and footer components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css-components/App.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Testing</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
