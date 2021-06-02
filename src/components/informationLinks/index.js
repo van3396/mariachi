c1a5e445a026e25009c52bcf044829af263fef86
Qui <quivan3396@yahoo.com>
2021-06-02 01:19:45 -0400

    "feat: add links and background image on informationLinks component"

---
+++ src/components/informationLinks/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from "./index.module.css";
+// import mariachi from "../../media/images/mariachi.jpg"
+
+const index = () => {
+    return (
+        <div className={`${style.main}`}>
+            <div className={`${style.links}`}>
+                <div>About Us</div>
+                <div>test1</div>
+                <div>test2</div>
+            </div>
+        </div>
+    )
+}
+
+export default index
